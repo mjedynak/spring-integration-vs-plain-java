c38b8b1f64dc13ac69116f2229f03fa15a6f6cbf
Michal Jedynak <m.jedynak@gmail.com>
2013-03-18 21:45:13 +0100

    renaming

--- src/main/java/pl/mjedynak/plain/App.java
+++ src/main/java/pl/mjedynak/plain/App.java
@@ -3,8 +3,8 @@ package pl.mjedynak.plain;
 public class App {
 
     public static void main(String[] args) {
-        MessageRouter consumer = new MessageRouter();
-        consumer.route("{'json'}");
-        consumer.route("<?xml/>");
+        MessageRouter router = new MessageRouter();
+        router.route("{'json'}");
+        router.route("<?xml/>");
     }
 }
