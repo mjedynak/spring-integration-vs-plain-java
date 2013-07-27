d61378eb178f35c9cd850936ad05edca4fb4ed96
Michal Jedynak <m.jedynak@gmail.com>
2013-07-27 22:59:11 +0200

    exported spring integration beans via jmx

--- src/main/java/pl/mjedynak/spring/App.java
+++ src/main/java/pl/mjedynak/spring/App.java
@@ -5,12 +5,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.integration.MessageChannel;
 import org.springframework.integration.support.MessageBuilder;
 
+import java.io.IOException;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
         MessageChannel channel = context.getBean("messageChannel", MessageChannel.class);
         channel.send(MessageBuilder.withPayload("{'json'}").build());
         channel.send(MessageBuilder.withPayload("<?xml/>").build());
+        System.in.read();
     }
 }
